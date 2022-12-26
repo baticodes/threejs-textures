420377a099fa29c45051bc4cfa077db2a228e0f9
David Bautista <david.bautista@hanzo.es>
2022-12-26 11:32:22 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,295 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+/**
+ * Base
+ */
+// Config
+const backgroundColor = 0xf1f1f1;
+
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(backgroundColor);
+// scene.fog = new THREE.Fog(backgroundColor, 60, 100);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+/**
+ * Models
+ */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
+
+gltfLoader.load(
+  '/models/Baby_old/glTF/BABY.gltf',
+  (oldGltf) => {
+    const oldModel = oldGltf.scene;
+    let shirtData = null;
+    let shirtMaterial = null;
+    console.log(oldModel);
+    oldModel.traverse(o => {
+      if (o.isMesh) {
+        if(o.name === 'Baby_Low_Poly_001_Baked') {
+          shirtData = o.toJSON();
+          shirtMaterial = o.material;
+
+          console.log(shirtMaterial.map);
+          console.log(shirtData.textures[0].wrap);
+        }
+      }
+    });
+
+    gltfLoader.load(
+      '/models/baby/baby.glb',
+      (gltf) => {
+        const model = gltf.scene;
+        const {children} = model;
+        // gltf.scene.scale.set(0.025, 0.025, 0.025)
+        scene.add(gltf.scene)
+        // console.log(children);
+  
+        model.traverse(o => {
+  
+          if (o.isMesh) {
+            o.castShadow = true;
+            o.receiveShadow = true;
+            // o.material = stacy_mtl;
+            if(o.name === 'Baby_Low_Poly_001_Baked') {
+              const material = o.material;
+              // console.log(material);
+              // console.log(material.color);
+            }
+            
+          }
+  
+          if(o.name === 'shirt') {
+            const texture = new THREE.TextureLoader().load( '/models/baby/shirt.png' );
+            texture.wrapS = THREE.ClampToEdgeWrapping
+            texture.wrapT = THREE.RepeatWrapping
+            texture.anisotropy = 1
+            texture.generateMipmaps = true
+            texture.isRenderTargetTexture = false
+            texture.magFilter = THREE.LinearFilter
+            texture.mapping = 300
+            texture.matrixAutoUpdate = true
+            texture.minFilter = 1008
+            
+            o.material = new THREE.MeshStandardMaterial({
+              color: new THREE.Color( 16777215 ),
+              metalness: 0,
+              roughness: 0.5,
+              map: texture,
+            })
+            // o.material = shirtMaterial;
+            // o.material = new THREE.MeshPhongMaterial({
+            //   map: texture,
+            //   color: new THREE.Color( 0xfff ),
+            //   skinning: true });
+
+            texture.needsUpdate = true
+            o.material.needsUpdate = true
+
+          }
+        });
+      }
+  )
+  }
+);
+
+// gltfLoader.load(
+//     // '/models/baby/baby.glb',
+//     '/models/Baby_old/glTF/BABY.gltf',
+//     (gltf) =>
+//     {
+//       const model = gltf.scene;
+//       const {children} = model;
+//       // gltf.scene.scale.set(0.025, 0.025, 0.025)
+//       scene.add(gltf.scene)
+//       console.log(children);
+
+//       model.traverse(o => {
+
+//         if (o.isMesh) {
+//           o.castShadow = true;
+//           o.receiveShadow = true;
+//           // o.material = stacy_mtl;
+//           if(o.name === 'Baby_Low_Poly_001_Baked') {
+//             const material = o.material;
+//             console.log(material);
+//             console.log(material.color);
+//           }
+          
+//         }
+
+//         if(o.name === 'shirt') {
+//           const texture = new THREE.TextureLoader().load( '/models/baby/shirt.png' );
+//           o.material = new THREE.MeshStandardMaterial({
+//               metalness: 0,
+//               roughness: 0.5,
+//               map: texture,
+//           })
+//           // o.material = new THREE.MeshPhongMaterial({
+//           //   map: texture,
+//           //   color: new THREE.Color( 0xfff ),
+//           //   skinning: true });
+//         }
+//       });
+//     }
+// )
+
+/**
+ * Floor
+ */
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial({
+        color: '#444444',
+        metalness: 0,
+        roughness: 0.5
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.54)
+directionalLight.position.set(-8, 12, 8);
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(- 5, 5, 0)
+scene.add(directionalLight)
+
+
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+// hemiLight.position.set(0, 50, 0);
+// // Add hemisphere light to scene
+// scene.add(hemiLight);
+
+// const d = 8.25;
+// const dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+// dirLight.position.set(-8, 12, 8);
+// dirLight.castShadow = true;
+// dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+// dirLight.shadow.camera.near = 0.1;
+// dirLight.shadow.camera.far = 1500;
+// dirLight.shadow.camera.left = d * -1;
+// dirLight.shadow.camera.right = d;
+// dirLight.shadow.camera.top = d;
+// dirLight.shadow.camera.bottom = d * -1;
+// // Add directional Light to scene
+// scene.add(dirLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// camera.position.set(2, 2, 2)
+// scene.add(camera)
+
+const camera = new THREE.PerspectiveCamera(
+  50,
+  sizes.width / sizes.height,
+  0.1,
+  1000);
+
+  camera.position.z = 2;
+  camera.position.x = 0;
+  camera.position.y = .8;
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0.75, 0)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // Model animation
+    if(mixer)
+    {
+        mixer.update(deltaTime)
+    }
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
